d2fd0d021ab96ce78a84005355be62eecfd344ae
Petar Simidzija <psimidzija@gmail.com>
2024-10-24 19:07:03 -0700

    improve parser

--- src/parse.py
+++ src/parse.py
@@ -10,20 +10,21 @@ with open('/Users/petar/Dropbox/llmdata/data/crawl_data.jsonl', 'r') as file:
     html = line['text']
 
 
-
 class Parser:
     def __init__(self):
+        self.allowed_tags = ['div', 'p', 'ol', 'ul', 'blockquote']
         self.end_ids = set(["See_also", 
                             "References", 
                             "Further_reading",
                             "External_links"]) 
-        self.boundary_classes = set(["mw-headings2", 
-                                     "mw-headings3"])
+        self.boundary_classes = set(["mw-heading2", 
+                                     "mw-heading3"])
     # TODO: Add logger
 
 
     def parse(self, html: str):
-        soup = BeautifulSoup(html, 'html.parser')
+        # use html5lib parser because it matches chrome better than html.parser
+        soup = BeautifulSoup(html, 'html5lib')
         main_tag = (soup
             .find('div', id="mw-content-text")
             .find('div', class_="mw-content-ltr mw-parser-output", lang="en")
@@ -32,111 +33,91 @@ class Parser:
         text_list = []
         text = ""
 
-        for tag in main_tag.children:
-            print(tag.name)
+        # print(main_tag.prettify())
+
+        for tag in main_tag.find_all(self.allowed_tags, recursive=False):
             if self.is_end(tag):
                 break
             elif self.is_new_section(tag):
                 if text:
-                    text_list.append(text)
+                    text_list.append(text.strip())
                 text = ""
                 continue
             else:
                 text += self.get_text(tag)
         
         if text:
-            text_list.append(text)
-                
+            text_list.append(text.strip())
 
-        print(text_list)
+        for sec in text_list:
+            print(sec)
+            print('\n')
 
         return text_list       
+
+
+    def get_text(self, tag: Tag):
+        if tag.name == 'p':
+            text = tag.text
+        elif tag.name == 'ol':
+            text = ""
+            for i, item in enumerate(tag.find_all('li', recursive=False), 1):
+                text += f"  {i}. {item.text}"
+        elif tag.name == 'ul':
+            text = ""
+            for item in tag.find_all('li', recursive=False):
+                text += f"  • {item.text}"
+        elif tag.name == 'blockquote':
+            if "templatequote" in tag.get("class", []):
+                text = f'\n{tag.text.strip()}\n'
+            else:
+                text = f'\n"{tag.text.strip()}"\n'
+        else:
+            text = ""
+        
+        return text
+        # TODO: other tags: 
+        # TODO: deal with equations
+        # TODO: get rid of refs (eg [2])
+        # TODO: get rid of [further explanation needed]
+        # TODO: get rid of [citation needed]
+
 
     def is_end(self, tag: Tag):
         if tag.name != "div":
             return False
 
         first_child = next(tag.children, None)
-        if first_child is None:
-            return False
-        elif first_child.name == "h2":
-            ids = set(tag.get("id", []))
-            return self.end_ids.union(ids)
+        if first_child and first_child.name == "h2":
+            id_ = first_child.get("id")
+            return id_ in self.end_ids
         else:
             return False
-
 
     def is_new_section(self, tag: Tag):
         if tag.name != "div":
             return False
         else:
             classes = set(tag.get("class", []))
-            return self.boundary_classes.union(classes)
+            return self.boundary_classes.intersection(classes)
 
-
-
-    def get_text(self, tag: Tag):
-        if tag.name == 'p':
-            return tag.text
-        else:
-            return ""
-
-        # TODO: deal with other cases (eg blockquote, li, etc)
-        # TODO: deal with equations
-        # TODO: get rid of refs (eg [2])
-        # TODO: get rid of [further explanation needed]
-
-
-
-    # def parse_intro(self, tag: Tag):
-    #     def is_intro_tag(child):
-    #         # True iff child is 1st level tag before first subheading
-    #         result = (child.parent == tag and 
-    #                   child.find_previous('div', class_="mw-heading2") is None)
-    #         return result
-        
-    #     intro_tags = tag.find_all(is_intro_tag)
-
-    #     text = ""
-    #     for par in intro_tags:
-    #         text += self.get_text(par)
-
-    #     return [text]
-
-    # def parse_main(self, tag: Tag):
-
-
-
-    # def parse(self, html: str):
-    #     soup = BeautifulSoup(html, 'html.parser')
-    #     contents = (soup
-    #         .find('div', id="mw-content-text")
-    #         .find('div', class_="mw-content-ltr mw-parser-output", lang="en")
-    #     )
-
-    #     text = []
-        
-    #     # Parse intro section
-    #     text.extend(self.parse_intro(contents))
-
-    #     # Parse other sections (h2 level)
-    #     text.extend(self.parse_main(contents))
-
-
-    #     for section in contents.find_all('div', class_="mw-heading2"):
-    #         print(section.name)
-    #         text.extend(self.get_text(section))
-
-    #     for i, subtext in enumerate(text, 1):
-    #         print(f'Subtext {i}:\n')
-    #         print(subtext)
-    #         print('\n')
-
-
-
-
-        
         
 
 parser = Parser()
 parser.parse(html)
+
+
+# import requests
+# from bs4 import BeautifulSoup
+
+# response = requests.get("https://en.wikipedia.org/wiki/Ethics_in_religion")
+# html = response.text
+
+# soup = BeautifulSoup(html, 'html5lib')
+# root = (soup
+#         .find('div', id="mw-content-text")
+#         .find('div', class_="mw-content-ltr mw-parser-output", lang="en")
+#         )
+
+# for tag in root.find_all(recursive=False):
+#     print(tag.name)
