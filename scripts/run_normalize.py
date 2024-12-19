fea07d36a1b814201cb3362daf3a265a7c023c7e
Petar <psimidzija@gmail.com>
2024-12-18 20:00:27 -0500

    Normalize text

--- scripts/run_normalize.py
+++ scripts/run_normalize.py
@@ -7,7 +7,7 @@ sys.path.append(str(ROOT/'src'))
 from normalize import normalize_jsonl
 
 if __name__ == "__main__":
-    inpath = str(ROOT/'data/parse_data_5.jsonl')
-    outpath = str(ROOT/'data/normalize_data_5.jsonl')
+    inpath = str(ROOT/'data/parse_data.jsonl')
+    outpath = str(ROOT/'data/normalize_data.jsonl')
     normalize_jsonl([inpath, inpath], outpath, processes=10)
     
