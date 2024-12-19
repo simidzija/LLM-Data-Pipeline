0680912c36220bec37e7d52a162bacbae8aacbcd
Petar <psimidzija@gmail.com>
2024-12-18 19:18:44 -0500

    Increase log file size limit, implement concurrent log handler

--- src/logger.py
+++ src/logger.py
@@ -1,24 +1,45 @@
 import sys
 import logging
-from logging.handlers import RotatingFileHandler
+from concurrent_log_handler import ConcurrentRotatingFileHandler
 from pathlib import Path
 
 ROOT = Path(__file__).resolve().parent.parent
 sys.path.append(str(ROOT/'src'))
 
+
 class Logger:
+    _loggers = {}
+
     def __init__(self, name: str):
+        if name in Logger._loggers:
+            print('logger found')
+            self.logger = Logger._loggers[name]
+            return 
+
         self.filename = str(ROOT/f'log/{name}.log')
+        lock_file = Path(f'{self.filename}.__{name}.lock')
+        if lock_file.exists():
+            lock_file.unlink()
+        log_dir = ROOT/'log'
+        log_dir.mkdir(exist_ok=True)
+
         self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.INFO) 
 
-        self.handler = RotatingFileHandler(self.filename, maxBytes=10*2**20, backupCount=5)
+        if not self.logger.handlers:
+            self.logger.setLevel(logging.INFO)
+            self.handler = ConcurrentRotatingFileHandler(
+                self.filename, 
+                maxBytes=1024*1024*1024, 
+                backupCount=5
+            )
         
-        self.formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+            self.formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         
-        self.handler.setFormatter(self.formatter)
+            self.handler.setFormatter(self.formatter)
 
-        self.logger.addHandler(self.handler)
+            self.logger.addHandler(self.handler)
+        
+        Logger._loggers[name] = self.logger
 
     def info(self, msg: str, *args, **kwargs):
         self.logger.info(msg, *args, **kwargs)
