2273ab3a27c981637629c099d28806249872489e
Petar <psimidzija@gmail.com>
2024-12-30 10:20:38 -0500

    Implement length statistics function

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import os
 import yaml
 import json
 from pathlib import Path
+import numpy as np
 
 ROOT = Path(__file__).resolve().parent.parent
 sys.path.append(str(ROOT/'src'))
@@ -15,27 +16,33 @@ def load_json(path):
     with open(path, 'r') as file:
         return json.load(file)
 
-def combine_jsonl_files(old_paths: list[str], new_path: str, overwrite=False):
-    if os.path.exists(new_path) and not overwrite:
-        raise RuntimeError(f'path {new_path} already exists and overwrite is set to {overwrite}.')
+def get_jsonl_len_stats(path: str) -> tuple[float]:
+    """
+    Return stats dict about jsonl file, containing:
+      - mean number of paragraphs per article
+      - std dev of number of paragraphs per article
+      - mean number of characters per paragraph
+      - std dev of number of characters per paragraph
+    """
 
-    with open(new_path, 'w') as outfile:
-        for old_path in old_paths:
-            with open(old_path, 'r') as infile:
-                outfile.write(infile.read())
-                outfile.write('\n')
+    pars_per_article = []
+    chars_per_par = []
+    stats = {}
 
+    with open(path, 'r') as file:
+        for line in file:
+            entry = json.loads(line)
+            text_lst = entry["text_list"]
+            pars_per_article.append(len(text_lst))
+            chars_per_par.extend([len(par) for par in text_lst])
+        
+    stats['avg_pars_per_article'] = np.mean(pars_per_article)
+    stats['std_pars_per_article'] = np.std(pars_per_article)
+    stats['avg_chars_per_par'] = np.mean(chars_per_par)
+    stats['std_chars_per_par'] = np.std(chars_per_par)
 
+    return stats
 
-# if __name__ == '__main__':
-    # old_paths = []
-    # for i in range(1,6):
-    #     path = str(ROOT/f'data/parse_data_{i}.jsonl')
-    #     old_paths.append(path)
-    
-    # new_path = str(ROOT/'data/parse_data.jsonl')
-
-    # combine_jsonl_files(old_paths, new_path)
 
         
     
