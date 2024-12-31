1e898eb7cd837c9233f3a413d738f6fab0978c0f
Petar <psimidzija@gmail.com>
2024-12-31 17:39:18 -0500

    Implement MinHash

--- src/deduplicate.py
+++ src/deduplicate.py
@@ -9,30 +9,33 @@ sys.path.append(str(ROOT/'src'))
 from logger import Logger
 
 class Deduplicator:
-    def __init__(self, inpath, outpath, gram_size, sig_len, bin_size, sim_threshold):
+    def __init__(self, inpath, outpath, gram_len, signature_len, band_size, similarity_threshold):
         self.inpath = inpath
         self.outpath = outpath
-        self.gram_size = gram_size
-        self.sig_len = sig_len
-        self.bin_size = bin_size
-        self.threshold = sim_threshold
+        self.gram_len = gram_len
+        self.signature_len = signature_len
+        self.band_size = band_size
+        self.similarity_threshold = similarity_threshold
 
         self.min_hashes = {}
         self.lsh_dicts = []
         self.pars_to_remove = {}
 
+        self.min_hash_fns = [lambda x, s=s: mmh3.hash(x, s) for s in range(signature_len)]
+        self.lsh_hash_fn = lambda x: mmh3.hash(x, len(signature_len))
+
     def deduplicate(self):
         """Deduplicates infile and writes to outfile"""
 
         # MinHash
-        self.min_hash()
+        self.min_hash_jsonl()
 
         # Locality-Sensitive Hashing
         self.lsh_create_dicts()
         self.lsh_get_pars_to_remove()
 
         # Create outfile
-        with open(self.outpath, 'r') as infile, open(self.outpath, 'w') as outfile:
+        with open(self.inpath, 'r') as infile, open(self.outpath, 'w') as outfile:
             for line in infile:
                 entry = json.loads(line)
                 url = entry['url']
@@ -44,11 +47,33 @@ class Deduplicator:
                 json.dump(entry, outfile)
                 outfile.write('\n')
                     
-
-
-    def min_hash(self):
+    def min_hash_jsonl(self):
         """Creates min_hashes dict"""
-        pass
+        with open(self.inpath, 'r') as infile:
+            for line in infile:
+                entry = json.loads(line)
+                url = entry['url']
+                text_list = entry['text_list']
+
+                self.min_hashes[url] = [self.min_hash(text) for text in text_list]
+
+    def min_hash(self, text: str) -> list[int]:
+        """Return MinHash signature of text"""
+        assert len(text) >= self.gram_len, f"len(text) ({len(text)}) cannot be smaller than gram_len ({self.gram_len})"
+
+        n_grams = set()
+        for start in range(len(text) - self.gram_len + 1):
+            end = start + self.gram_len
+            n_gram = text[start:end]
+            n_grams.add(n_gram)
+
+        signature = []
+        for fn in self.min_hash_fns:
+            min_hash_val = min(map(fn, n_grams))
+            signature.append(min_hash_val)
+
+        return signature
+
 
     def lsh_create_dicts(self):
         """Creates lsh_dicts list"""
